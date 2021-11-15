aa5ef52470690697902dadb315d91a92276d10f3
Mark Rebec <mark@markrebec.com>
2021-11-15 13:40:59 -0800

    fixes type exports in theme

--- lib/generators/greenhill/webpack/templates/app/javascript/theme/index.ts
+++ lib/generators/greenhill/webpack/templates/app/javascript/theme/index.ts
@@ -4,12 +4,9 @@ import { Theme } from 'styled-system'
 import { colors as primitiveColors, accents, typography, layout } from './primitives'
 import { Colors } from './primitives/colors'
 
-export {
-  Common, Border, Layout, Position, Flex, Grid,
-  CommonProps, BorderProps, LayoutProps, PositionProps, FlexProps, GridProps
-} from './constants'
-
 export { ThemeProvider } from './ThemeProvider'
+export { Common, Border, Layout, Position, Flex, Grid } from './constants'
+export type { CommonProps, BorderProps, LayoutProps, PositionProps, FlexProps, GridProps } from './constants'
 
 export interface CustomTheme extends Theme {
   colors: Colors
