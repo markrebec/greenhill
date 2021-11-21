9b0fa1281bd544f897f2b69984507e282d95ed65
Mark Rebec <mark@markrebec.com>
2021-11-20 18:35:17 -0800

    automatically adds JWT header to graphiql to set admin to user

--- lib/generators/greenhill/graphql/templates/config/initializers/graphql.rb
+++ lib/generators/greenhill/graphql/templates/config/initializers/graphql.rb
@@ -1,2 +1,7 @@
-# NOTE this option currently requires using the 'master' branch of graphiql-rails from github
-GraphiQL::Rails.config.header_editor_enabled = true
+# NOTE this option currently requires using the 'master' branch of graphiql-rails from github...
+#      would be a nice addition once it's released
+# GraphiQL::Rails.config.header_editor_enabled = true
+
+GraphiQL::Rails.config.headers['Authorization'] = -> (context) {
+  "Bearer #{Warden::JWTAuth::UserEncoder.new.call(context.current_admin, :user, Warden::JWTAuth::EnvHelper.aud_header({}))[0]}"
+}
